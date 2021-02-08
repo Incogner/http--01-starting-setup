c8520e96732a377306d9aace7e2a4961caeb9482
Nanoteknology <36832557+Nanoteknology@users.noreply.github.com>
2021-02-08 00:04:28 -0500

    create posts module

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,7 +1,56 @@
 import React, { Component } from 'react';
+import axios from '../../../axios';
 
-const posts = (props) => (
+import Post from '../../../components/Post/Post';
+import './Posts.css';
 
-);
+class Posts extends Component {
 
-export default posts;
+    state = {
+        posts: []
+    };
+
+    componentDidMount() {
+        axios.get('/posts')
+        .then(response => {
+            const posts = response.data.slice(0, 4);
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'Max'
+                }
+            });
+            this.setState({ posts: updatedPosts});
+        })
+        .catch( error => {
+            this.setState({ error: true})
+        });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
+    render () {
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
+        if(!this.state.error){
+            posts = this.state.posts.map(
+                post => {
+                    return <Post 
+                                key={post.id}
+                                title={post.title}
+                                clicked={() => this.postSelectedHandler(post.id)} 
+                                author={post.author}/>
+                }
+            );
+        }
+
+        return (
+            <section className="Posts">
+                    {posts}
+            </section>
+        );
+    }
+}
+
+export default Posts;
