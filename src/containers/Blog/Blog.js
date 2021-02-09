36a6e3713d6b896c95b622901455e308c8ed23a9
Nanoteknology <36832557+Nanoteknology@users.noreply.github.com>
2021-02-08 23:43:12 -0500

    dynamic nested route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,6 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -16,13 +15,13 @@ class Blog extends Component {
                         <ul>
                             {/* this.props.match.url + path => relative path */}
                             <li><NavLink 
-                                    to="/" 
+                                    to="/posts/" 
                                     exact 
                                     activeClassName="my-active"
                                     activeStyle={{
                                         color: '#fa923f',
                                         textDecoration: 'underline'
-                                    }}>Home</NavLink></li>
+                                    }}>Posts</NavLink></li>
                             <li><NavLink to={{
                                     pathname: '/new-post',
                                     hash: 'submit',
@@ -32,9 +31,8 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    <Route path="/" exact component={Posts} />
                     <Route path="/new-post" component={NewPost} />
-                    <Route path="/:id" exact component={FullPost} />
+                    <Route path="/posts" component={Posts} />
                 </Switch>
             </div>
         );
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
-//import { Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import FullPost from '../FullPost/FullPost';
 
 import Post from '../../../components/Post/Post';
 import './Posts.css';
@@ -29,8 +30,8 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        //this.props.history.push({pathname: '/' + id});
-        this.props.history.push('/' + id);
+        //this.props.history.push({pathname: '/posts/' + id});
+        this.props.history.push('/posts/' + id);
     }
 
     render () {
@@ -39,7 +40,7 @@ class Posts extends Component {
             posts = this.state.posts.map(
                 post => {
                     return (
-                    //<Link to={'/'+post.id} key={post.id}>
+                    //<Link to={'/posts/'+post.id} key={post.id}>
                         <Post 
                             key={post.id}
                             title={post.title}
@@ -52,9 +53,12 @@ class Posts extends Component {
         }
 
         return (
-            <section className="Posts">
+            <div>
+                <section className="Posts">
                     {posts}
-            </section>
+                </section>
+                <Route path={this.props.match.url + '/:id'} exact component={FullPost} />
+            </div>
         );
     }
 }
